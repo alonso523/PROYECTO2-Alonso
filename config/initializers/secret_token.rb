98a2a3556a3c7c91a8698d2a87e23b1e27270d16
alonso523 <alonso523@outlook.com>
2012-10-16 23:49:09 -0600

    Primer commit de la segunda tarea programada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebTp2Alonso::Application.config.secret_token = '3b4652f638fbf476f0c1bb8f2cbb6a1f0cbf7b20e78c8ff39ec70e2fb223ea8662e1daf3fe441a2e7d9d3d8844ac40a42dc4988fc5d0dd4142dab490d0bf640c'
